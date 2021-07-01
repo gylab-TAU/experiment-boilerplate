9eb3bd7a302964ed2c47cbe7936a7f823774f753
Gali Geller <gali1998@gmai.com>
2021-07-01 22:57:26 +0300

    Initialised next js

---
+++ pages/_app.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import '../styles/globals.css'
+
+function MyApp({ Component, pageProps }) {
+  return <>
+    <Head>
+      <title>Create Next App</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <Component {...pageProps} />
+  </>
+}
+
+export default MyApp
